ee2e6e6bbc84b6dfc827c68230770ac349c3916a
Vicka <vicrogatinskaya@gmail.com>
2022-09-07 14:17:40 +0200

    som changes

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,11 +5,29 @@ import { Heading1, Heading2, SectionTitle, Texts } from "../Text";
 
 export const Cards = () => {
     return (
+        <div className={s.cardline}>
+
+            <div class={s.card}>
+                <img src="http://placekitten.com/200/200" className={s.cardImg}/>
+                <div class={s.container}>
+                <Texts>Woooof</Texts>
+                </div>
+            </div>
+
         <div class={s.card}>
-        <img src="http://placekitten.com/200/200" />
-        <div class={s.container}>
-          <Texts>Woooof</Texts>
+            <img src="http://placekitten.com/200/200" className={s.cardImg}/>
+            <div class={s.container}>
+            <Texts>Woooof</Texts>
+            </div>
         </div>
+
+        <div class={s.card}>
+            <img src="http://placekitten.com/200/200" className={s.cardImg}/>
+            <div class={s.container}>
+            <Texts>Woooof</Texts>
+            </div>
+        </div>
+
       </div> 
     );
 };
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,7 +8,7 @@ export const Landing = () => {
             <img src={"assets/dog1.png"} className={s.img} />
             <img src={"assets/blob.svg"} className={s.blob} />
             <img src={"assets/blob1.svg"} className={s.blob1} />
-            <Spacer size={64} />
+            {/* <Spacer size={64} /> */}
             <Heading1>hi</Heading1>
             <Heading2>my name is Victoria</Heading2>
             <Texts>i'm a front end dev</Texts>
