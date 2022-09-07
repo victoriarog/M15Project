d162be3d9aee60634012c5689c6d5aa2f20f5090
Vicka <vicrogatinskaya@gmail.com>
2022-09-07 13:58:54 +0200

    blobsss

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,10 +6,12 @@ export const Landing = () => {
     return (
             <div className={s.introtxt}>
             <img src={"assets/dog1.png"} className={s.img} />
+            <img src={"assets/blob.svg"} className={s.blob} />
+            <img src={"assets/blob1.svg"} className={s.blob1} />
             <Spacer size={64} />
-            <Heading1>henlo</Heading1>
-            <Heading2>my name is ?!</Heading2>
-            <Texts>im a front end dev</Texts>
+            <Heading1>hi</Heading1>
+            <Heading2>my name is Victoria</Heading2>
+            <Texts>i'm a front end dev</Texts>
             </div>
             
     );  
