e6302f1860a152613446af31e17d4f5c029507c7
Vicka <vicrogatinskaya@gmail.com>
2022-09-08 02:57:17 +0200

    cards flip now woooo

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,41 +1,94 @@
 import s from "./Cards.module.css";
-import { Spacer } from "../Spacer";
 import { Heading1, Heading2, SectionTitle, Texts } from "../Text";
 import { Parallax } from 'react-scroll-parallax';
+import ReactCardFlip from "react-card-flip";
+import React, { useState } from "react";
 
 
 export const Cards = () => {
+    const [flip, setFlip] = useState(false);
+    const [flip1, setFlip1] = useState(false);
+    const [flip2, setFlip2] = useState(false);
+    const [flip3, setFlip3] = useState(false);
     return (
         <Parallax translateY={[-20, 20]} speed={5}>
         <div className={s.cardline}>
 
-            <div class={s.card}>
-                <img src={"assets/dog2.png"} className={s.cardImg}/>
+        <ReactCardFlip isFlipped={flip} 
+            flipDirection="vertical">
+            <div class={s.card} onClick={() => setFlip(!flip)}>
                 <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
                 <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
                 </div>
             </div>
 
-        <div class={s.card}>
-            <img src={"assets/dog2.png"} className={s.cardImg}/>
-            <div class={s.container}>
-            <Texts>Woooof</Texts>
+            <div class={s.card} onClick={() => setFlip(!flip)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
             </div>
-        </div>
+            </ReactCardFlip>
 
-        <div class={s.card}>
-            <img src={"assets/dog2.png"} className={s.cardImg}/>
-            <div class={s.container}>
-            <Texts>Woooof</Texts>
+            <ReactCardFlip isFlipped={flip1} 
+            flipDirection="vertical">
+            <div class={s.card} onClick={() => setFlip1(!flip1)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
             </div>
-        </div>
 
-        <div class={s.card}>
-            <img src={"assets/dog2.png"} className={s.cardImg}/>
-            <div class={s.container}>
-            <Texts>Woooof</Texts>
+            <div class={s.card} onClick={() => setFlip1(!flip1)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
             </div>
-        </div>
+            </ReactCardFlip>
+
+            <ReactCardFlip isFlipped={flip2} 
+            flipDirection="vertical">
+            <div class={s.card} onClick={() => setFlip2(!flip2)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
+            </div>
+
+            <div class={s.card} onClick={() => setFlip2(!flip2)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
+            </div>
+            </ReactCardFlip>
+
+            <ReactCardFlip isFlipped={flip3} 
+            flipDirection="vertical">
+            <div class={s.card} onClick={() => setFlip3(!flip3)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
+            </div>
+
+            <div class={s.card} onClick={() => setFlip3(!flip3)}>
+                <div class={s.container}>
+                <img src={"assets/dog2.png"} className={s.cardImg}/>
+                <Texts>Woooof</Texts>
+                <p className={s.ct}>woof woof woof</p>
+                </div>
+            </div>
+            </ReactCardFlip>
 
       </div> 
       </Parallax>
